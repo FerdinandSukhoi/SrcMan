f103dfa0e1aa73b9870d307a98300bb69a4a3e37
宿梓航 <Ferdinandsukhoi@outlook.com>
2019-10-30 17:57:07 +0800

    添加项目文件。

---
+++ SrcMan/MobileSuit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace SrcMan
+{
+    public class MobileSuit
+    {
+        public Assembly Assembly { get; set; }
+        public Stream Stream { get; set; }
+        public MobileSuit()
+        {
+            Assembly = Assembly.GetExecutingAssembly();
+        }
+
+        public int Run(string prompt)
+        {
+
+            for (; ; )
+            {
+                string cmd = Console.ReadLine();
+            }
+
+            return 0;
+        }
+
+        public int Run()
+        {
+
+            for(; ; )
+            {
+                string cmd = Console.ReadLine();
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ SrcMan/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace SrcMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            var m = assembly.GetType("SrcMan.Test").GetMethod("testM");
+            m.Invoke();
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
